4a6717941a9ab2f50ce9a44e0094d8058d9a3023
ixiazer <shan.xu@freshfresh.com>
2016-07-13 13:57:02 +0800

    Initial Commit

---
+++ FFMutAndBackgroundDownload/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FFMutAndBackgroundDownload
+//
+//  Created by ixiazer on 16/7/13.
+//  Copyright © 2016年 FF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FFMutAndBackgroundDownload/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FFMutAndBackgroundDownload
+//
+//  Created by ixiazer on 16/7/13.
+//  Copyright © 2016年 FF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
